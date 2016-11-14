3122f031e6e7d572c08b184e8890a139fb0e1d1f
puppee <pupp3@icloud.com>
2016-11-14 13:08:12 +0200

    Create login.js

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mocextranetApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the mocextranetApp
+ */
+angular.module('mocextranetApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
