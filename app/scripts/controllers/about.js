0d69bf5d7e13de94a00eedfaa256af8fc6ec2084
Tuomas <tuomas@puppe.me>
2016-11-14 12:13:48 +0200

    Eka

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mocextranetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mocextranetApp
+ */
+angular.module('mocextranetApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mocextranetApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mocextranetApp
+ */
+angular.module('mocextranetApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
